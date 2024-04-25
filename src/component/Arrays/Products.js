3d445d5d48dc8ef3984501f5f07f3d4cba7c238a
AdisaDuke <rasakadisa.fcaib@gmail.com>
2024-04-25 14:45:02 +0100

    arrays

--- src/component/Arrays/Products.js
+++ src/component/Arrays/Products.js
@@ -1,13 +1,12 @@
 import product1 from "../Assets/image 6.png";
 import product2 from "../Assets/image 7.png";
-import product3 from "../Assets/image 7.png";
-import product4 from "../Assets/image 8.png";
-import product5 from "../Assets/image 9.png";
-import product6 from "../Assets/image 10.png";
-import product7 from "../Assets/image 11.png";
-import product8 from "../Assets/image 12.png";
-import product9 from "../Assets/image 13.png";
-import product10 from "../Assets/image 14.png";
+import product3 from "../Assets/image 8.png";
+import product4 from "../Assets/image 9.png";
+import product5 from "../Assets/image 10.png";
+import product6 from "../Assets/image 11.png";
+import product7 from "../Assets/image 12.png";
+import product8 from "../Assets/image 13.png";
+import product9 from "../Assets/image 14.png";
 
 const PRODUCT = [
   {
@@ -19,7 +18,7 @@ const PRODUCT = [
   },
   {
     id: 2,
-    product_name: "Skinny Fit Jeans",
+    product_name: " Round T-shirt",
     image: product2,
     ratings: "★★★★★",
     price: "240",
@@ -28,14 +27,14 @@ const PRODUCT = [
   },
   {
     id: 3,
-    product_name: "Checkered Shirt",
+    product_name: "Skinny Fit Jeans",
     image: product3,
     ratings: "★★★★★",
     price: "180",
   },
   {
     id: 4,
-    product_name: "Sleeve Striped T-shirt",
+    product_name: "Checkered Shirt",
     image: product4,
     ratings: "★★★",
     price: "130",
@@ -44,8 +43,8 @@ const PRODUCT = [
   },
   {
     id: 5,
-    product_name: "Skinny Fit Jeans",
-    image: product9,
+    product_name: "Stripped Round Neck Shirt",
+    image: product5,
     ratings: "★★★",
     price: "240",
     previous_price: "$260",
@@ -53,15 +52,15 @@ const PRODUCT = [
   },
   {
     id: 6,
-    product_name: "Checkered Shirt",
-    image: product3,
+    product_name: "Black Skinny Fit Jeans",
+    image: product6,
     ratings: "★★★★★",
     price: "180",
   },
   {
     id: 7,
-    product_name: "Skinny Fit Jeans",
-    image: product6,
+    product_name: "Short Jeans",
+    image: product7,
     ratings: "★★★★",
     price: "240",
     previous_price: "$260",
@@ -69,33 +68,104 @@ const PRODUCT = [
   },
   {
     id: 8,
-    product_name: "Checkered Shirt",
-    image: product5,
+    product_name: "Round Shirt with Brand",
+    image: product8,
     ratings: "★★★★★",
     price: "180",
   },
   {
     id: 9,
-    product_name: "Checkered Shirt",
-    image: product10,
+    product_name: "Sleeve Striped T-shirt",
+    image: product9,
     ratings: "★★★",
     price: "180",
   },
   {
     id: 10,
-    product_name: "Sleeve Striped T-shirt",
-    image: product7,
+    product_name: "Jersey Bomber Jacket",
+    image: "https://i.ibb.co/L1NmKWk/asos-jersey-bomber.png",
     ratings: "★★★★",
-    price: "130",
+    price: "230",
     previous_price: "$160",
-    discount: "-30%",
+    discount: "-70%",
   },
   {
     id: 11,
-    product_name: "T-shirt with Tape Details",
-    image: product8,
+    product_name: "Plain Grey Unisex Hoodie",
+    image: "https://i.ibb.co/vQjr87H/Plain-Grey-unisex-hoodie.png",
     ratings: "★★★★★",
     price: "120",
+  },
+  {
+    id: 12,
+    product_name: "Men Hoodie Grey",
+    image: "https://i.ibb.co/6JFswQx/119736-1700748078.png",
+    ratings: "★★★★",
+    price: "180",
+    previous_price: "$160",
+    discount: "-20%",
+  },
+  {
+    id: 13,
+    product_name: "Dark Red Round Neck Plain T-Shirt",
+    image: "https://i.ibb.co/HHZ88rj/sh.png",
+    ratings: "★★★★",
+    price: "200",
+    previous_price: "$180",
+    discount: "-20%",
+  },
+  {
+    id: 14,
+    product_name: "Multicoloured Hoodie",
+    image: "https://i.ibb.co/Jc3fpmr/139554-1563383444.png",
+    ratings: "★★★★",
+    price: "200",
+  },
+  {
+    id: 15,
+    product_name: "Classic Combat Pant Trousers",
+    image: "https://i.ibb.co/5B8GF6f/IMG-20201008-191825.jpg",
+    ratings: "★★★★",
+    price: "250",
+    previous_price: "$200",
+    discount: "-50%",
+  },
+  {
+    id: 16,
+    product_name: "SML Men Cargo Shorts Black",
+    image: "https://i.ibb.co/jgK11Sy/cargo-shorts-1.png",
+    ratings: "★★★★",
+    price: "250",
+    previous_price: "$180",
+    discount: "-70%",
+  },
+  {
+    id: 17,
+    product_name: "Track Pants for Male and Female",
+    image: "https://i.ibb.co/R0tkD4R/Track-pants-600x586.png",
+    ratings: "★★★★",
+    price: "180",
+  },
+  {
+    id: 18,
+    product_name: "Red Striped Track Pants",
+    image: "https://i.ibb.co/g4wXbJB/Red-and-black-track-pants.png",
+    ratings: "★★★★",
+    price: "180",
+  },
+  {
+    id: 19,
+    product_name: "Crew Clothing Cargo Joggers",
+    image: "https://i.ibb.co/3myG39L/1-71.png",
+    ratings: "★★★★",
+    price: "280",
+  },
+  {
+    id: 20,
+    product_name: "Plain Grey Unisex Hoodie",
+    image: "https://i.ibb.co/D7hNySF/Plain-Grey-unisex-hoodie.png",
+    ratings: "★★★★",
+    price: "250",
   },
 ];
 
